2feb7b2356aaf16e915831196df6f5248b95bdb0
ggobbe <guillaume.gobbe@gmail.com>
2015-11-17 17:15:20 +0000

    Use a constant for the upper boundary

--- 1-ruby/day1_bonus.rb
+++ 1-ruby/day1_bonus.rb
@@ -1,16 +1,20 @@
 #!/usr/bin/ruby
 
-max = 10
-num = rand(max)
+# Bonus problem: If you're feeling the need for a little more, write a program
+# that picks a random number. Let a player guess the number, telling the player
+# whether the guess is too low or too high
 
-puts "Try to find the number between 0 and #{max}!"
+MAX = 10
+num = rand(MAX)
+
+puts "Try to find the number between 0 and #{MAX}!"
 
 print "Guess: "
 while line = gets do 
     try = line.to_i
     case try 
         when 0...num then puts 'Your guess is too low...'
-        when num+1..max then puts 'Your guess it too high...'
+        when num+1..MAX then puts 'Your guess it too high...'
         when num then
             puts 'You won!!!' 
             break
